0c7dabfca1584acc4961391d7d8ff22dfc31be36
Burze <aleksandar.burzevski@cosmicdevelopment.com>
2014-07-25 21:34:40 +0200

    jQuery validation for the subscribe form

--- js/main.js
+++ js/main.js
@@ -7,6 +7,25 @@ $(document).ready(function(){
       $(".menu-box").slideToggle("fast");
   });
    $(".slider1").bxSlider({controls:false, pagerCustom: '#bx-pager', touchEnabled: true});
+// burze
+    $(".sub_in.btn").click(function(){
+        var email = $('.email').val();
+        if (validateEmail(email)) {
+            $(".alert_subscribe").hide();
+            return true;
+        }
+        else {
+            $(".alert_subscribe").show();
+            return false;
+        }
+    });
+
+    function validateEmail(email) { 
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+// burze
+
 });
 
 $(window).load(function() {
